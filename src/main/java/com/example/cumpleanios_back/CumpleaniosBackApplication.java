b3a14a194c35d19e20735b708653c68fbba698a2
ldlignia23 <luislignia@gmail.com>
2024-09-16 15:11:02 -0500

    first commit

---
+++ src/main/java/com/example/cumpleanios_back/CumpleaniosBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.cumpleanios_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CumpleaniosBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CumpleaniosBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cumpleanios_back/CumpleaniosBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cumpleanios_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CumpleaniosBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
