970c93e50e45a18434c1dfa406ff67c4e02aba70
Fernando Sangopanta <jhon.sangopanta@epn.edu.ec>
2024-09-18 14:26:49 -0500

    refactor: list all users

--- src/main/java/com/example/cumpleanios_back/infrastructure/controller/UserController.java
+++ src/main/java/com/example/cumpleanios_back/infrastructure/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.example.cumpleanios_back.infrastructure.controller;
 
+import com.example.cumpleanios_back.application.services.EmailService;
 import com.example.cumpleanios_back.application.services.UserService;
+import com.example.cumpleanios_back.application.services.utils.EmailBody;
 import com.example.cumpleanios_back.application.usecases.FindAllByBirthMonth;
 import com.example.cumpleanios_back.application.usecases.FindEmployeesByBirthMonthUseCase;
+import com.example.cumpleanios_back.domain.entities.Role;
+import com.example.cumpleanios_back.domain.entities.RoleType;
 import com.example.cumpleanios_back.domain.entities.UserEntity;
+import com.example.cumpleanios_back.domain.repositories.RoleRepository;
 import com.example.cumpleanios_back.infrastructure.dto.user.UserBirthayDtoResponse;
 import com.example.cumpleanios_back.infrastructure.dto.user.UserCreateDtoRequest;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +20,10 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
@@ -22,12 +31,16 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    private final FindEmployeesByBirthMonthUseCase findEmployeesByBirthMonthUseCase;
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
     private final FindAllByBirthMonth findAllByBirthMonth;
 
     @Autowired
     public UserController(FindEmployeesByBirthMonthUseCase findEmployeesByBirthMonthUseCase, FindAllByBirthMonth findAllByBirthMonth) {
-        this.findEmployeesByBirthMonthUseCase = findEmployeesByBirthMonthUseCase;
         this.findAllByBirthMonth = findAllByBirthMonth;
     }
 
@@ -54,9 +67,27 @@ public class UserController {
         }
     }
 
-    @GetMapping("/indexUsers")
-    public List<UserEntity> indexUsers() {
-        return this.userService.indexUsers();
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        try {
+            List<UserEntity> users = this.userService.indexUsers();
+            users.stream().filter(userEntity -> userEntity.getRoles().contains(RoleType.EMPLOYEE)).collect(Collectors.toList());
+            List<UserCreateDtoRequest> response = new ArrayList<>();
+
+            for (UserEntity user : users) {
+                response.add(
+                        UserCreateDtoRequest.builder()
+                                .name(user.getName())
+                                .lastName(user.getLastName())
+                                .email(user.getLastName())
+                                .birthDate(user.getDateBirth())
+                                .build()
+                );
+            }
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/showUsers/{id}")
@@ -66,24 +97,28 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<?> createUser(@RequestBody UserCreateDtoRequest user) {
-        try{
+        try {
+            Role employeeRole = Role.builder().roleType(RoleType.EMPLOYEE).build();
+            employeeRole = roleRepository.save(employeeRole);
+
             var userEntityBuilder = UserEntity.builder().
                     name(user.name())
                     .email(user.email())
                     .dateBirth(user.birthDate())
                     .lastName(user.lastName())
+                    .roles(Set.of(employeeRole))
                     .build();
             UserEntity created = this.userService.createUser(userEntityBuilder);
 
-            var response =UserCreateDtoRequest.builder().
+            var response = UserCreateDtoRequest.builder().
                     name(created.getName())
                     .lastName(created.getLastName())
                     .email(created.getEmail())
                     .birthDate(created.getDateBirth())
                     .build();
 
-            return new ResponseEntity<>(response,HttpStatus.OK);
-        }catch (Exception e){
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
             return new ResponseEntity<>(e, HttpStatus.BAD_REQUEST);
         }
 
@@ -94,10 +129,19 @@ public class UserController {
         return this.userService.updateUser(id, users);
     }
 
-    @DeleteMapping("/deleteUsers/{id}")
+    @DeleteMapping("/{id}")
     public String deleteUser(@PathVariable Long id) {
         this.userService.deleteUser(id);
         return "Empleado eliminado con exito";
     }
 
+    @PostMapping("/send")
+    public ResponseEntity<?> sendMail(@RequestBody EmailBody emailBody) {
+        try {
+            var response = this.emailService.sendEmail(emailBody);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
